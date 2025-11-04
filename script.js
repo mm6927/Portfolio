a37d83f94138e75b19768797e0ec1aa5dfecba22
mm6927 <101319789+mm6927@users.noreply.github.com>
2025-11-04 11:38:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#typed", {
+  strings: [
+    "Software Developer",
+    "Full-Stack Web Developer",
+    "AI & ML Enthusiast",
+    "Data Analytics Learner"
+  ],
+  typeSpeed: 80,
+  backSpeed: 50,
+  loop: true
+});
